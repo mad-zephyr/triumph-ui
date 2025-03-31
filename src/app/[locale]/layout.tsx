5469ef10b1a0b7af230804f3cffa8a90038ececc
vasilii iatiuc <72764693+mad-zephyr@users.noreply.github.com>
2025-03-31 10:13:32 +0300

    update internationalizations

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,10 @@ import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { mainFont } from "@/assets/fonts/fonts";
 
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
 export default async function LocaleLayout({
   children,
   params,
