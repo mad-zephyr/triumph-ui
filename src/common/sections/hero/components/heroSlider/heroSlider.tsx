f6e873e03c1a8d74b603a222d93476721b1c4509
vasilii iatiuc <72764693+mad-zephyr@users.noreply.github.com>
2025-04-06 00:58:07 +0300

    update hearo slider progres bar

--- src/common/sections/hero/components/heroSlider/heroSlider.tsx
+++ src/common/sections/hero/components/heroSlider/heroSlider.tsx
@@ -2,10 +2,8 @@
 
 import 'react-splide-ts/css';
 
-import { useGSAP } from '@gsap/react';
 import clsx from 'clsx';
-import gsap from 'gsap';
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC, useRef, useState } from 'react';
 import { Splide, SplideTrack } from 'react-splide-ts';
 import { useMediaQuery } from 'usehooks-ts';
 
@@ -62,55 +60,54 @@ export const HeroSlider: FC = () => {
   const isMobile = useMediaQuery('(max-width: 40rem)');
 
   const splideInstanceRef = useRef<Splide | null>(null);
-  const [timeLine] = useState<gsap.core.Timeline>(gsap.timeline());
 
   const [navIndexes, setNavIndxes] = useState({
     prev: sliderData.length - 1,
     next: 1,
   });
 
-  const { contextSafe } = useGSAP({
-    dependencies: [timeLine, isMobile],
-    scope: progressRef,
-  });
+  const runProgressBar = () => {
+    if (progressRef.current) {
+      progressRef.current.style.transition = `${DURATION - 0.2}s all ease-in-out`;
+      if (isMobile) {
+        progressRef.current.style.width = '100%';
+      } else {
+        progressRef.current.style.height = '100%';
+      }
+    }
+  };
 
-  useEffect(() => {
-    timeLine.restart();
-  }, [isMobile, timeLine]);
+  // const handleRun = contextSafe(() => {
+  //   const progresBarSettings: [gsap.TweenVars, gsap.TweenVars] = isMobile
+  //     ? [
+  //         { y: '0%', x: '50%', width: '0dvw', height: '0.375rem' },
+  //         {
+  //           width: '100dvw',
+  //           duration: DURATION,
+  //           ease: 'power1.inOut',
+  //         },
+  //       ]
+  //     : [
+  //         { y: '-50%', height: 0 },
+  //         {
+  //           height: '100%',
+  //           duration: DURATION,
+  //           ease: 'power1.inOut',
+  //         },
+  //       ];
 
-  const handleRun = contextSafe(() => {
-    const progresBarSettings: [gsap.TweenVars, gsap.TweenVars] = isMobile
-      ? [
-          { y: '0%', x: '50%', width: '0dvw', height: '0.375rem' },
-          {
-            width: '100dvw',
-            duration: DURATION,
-            ease: 'power1.inOut',
-          },
-        ]
-      : [
-          { y: '-50%', height: 0 },
-          {
-            height: '100%',
-            duration: DURATION,
-            ease: 'power1.inOut',
-          },
-        ];
+  //   timeLine
+  //     ?.addLabel('progress')
+  //     .fromTo(progressRef.current, ...progresBarSettings);
+  // });
 
-    timeLine
-      ?.addLabel('progress')
-      .fromTo(progressRef.current, ...progresBarSettings);
-  });
-
-  const handlePause = contextSafe(() => {
-    timeLine.pause();
+  const handlePause = () => {
     splideInstanceRef.current?.splide?.Components.Autoplay?.pause();
-  });
+  };
 
-  const handlePlay = contextSafe(() => {
-    timeLine.play();
+  const handlePlay = () => {
     splideInstanceRef.current?.splide?.Components.Autoplay?.play();
-  });
+  };
 
   const getPrevAndNext = (active: number) => {
     if (active === 0) {
@@ -128,13 +125,25 @@ export const HeroSlider: FC = () => {
         ref={splideInstanceRef}
         className={classes.slider}
         hasTrack={false}
+        onMoved={() => {
+          runProgressBar();
+        }}
+        onResized={runProgressBar}
         onMove={(splide) => {
           getPrevAndNext(splide.index);
-          handleRun();
+
+          if (progressRef.current) {
+            progressRef.current.style.transition = '0s all ease-in-out';
+            if (isMobile) {
+              progressRef.current.style.width = '0%';
+            } else {
+              progressRef.current.style.height = '0%';
+            }
+          }
         }}
         onReady={(splide) => {
           splide.Components.Autoplay.play();
-          handleRun();
+          runProgressBar();
         }}
         options={{
           type: 'loop',
@@ -174,7 +183,13 @@ export const HeroSlider: FC = () => {
             </div>
             <ArrorRight />
           </button>
-          <div className={classes.progress} ref={progressRef} />
+
+          <div className={classes['carousel-progress']}>
+            <div
+              className={classes['carousel-progress-bar']}
+              ref={progressRef}
+            />
+          </div>
 
           <button
             className={clsx(
