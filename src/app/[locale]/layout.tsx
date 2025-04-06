2f770a33537c07cb9f6782bd62872d88b20bc02a
vasilii iatiuc <72764693+mad-zephyr@users.noreply.github.com>
2025-04-06 15:23:54 +0300

    Test base favicon and genereated metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { hasLocale, NextIntlClientProvider } from 'next-intl';
 
@@ -6,6 +7,26 @@ import { routing } from '@/i18n/routing';
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'Triumph Motocycle Moldova',
+    description:
+      'Join Triumph Motocycle Club for a premium fitness experience.',
+    openGraph: {
+      title: 'Triumph Moldova',
+      images: [{ url: '/public/og-image.jpg', height: 630, width: 1200 }],
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: 'Triumph Motocycle Club',
+      description:
+        'Join Triumph Motocycle Club for a premium fitness experience.',
+      images: '/public/og-image.jpg',
+    },
+  };
 }
 
 export default async function LocaleLayout({
